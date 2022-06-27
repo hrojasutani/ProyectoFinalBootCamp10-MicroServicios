a63a7aa64cf87635f5f6f64f76230a327523e812
Hrojas <hernanrojasutani@gmail.com>
2022-06-27 18:30:05 -0500

    Cambio

--- config-server/src/main/java/com/nttdata/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/nttdata/config/ConfigServerApplication.java
@@ -10,6 +10,7 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		// SpringApplication.run(ProyectoBootcampjpaApplication.class, new String[]{"--server.port=8001"});
 	}
 
 }
